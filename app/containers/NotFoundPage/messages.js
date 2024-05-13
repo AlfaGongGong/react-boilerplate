4ec1d5b1111274a966fe1992937dd7f69cb280b1
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2024-05-13 03:19:34 +0200

    chore: Remove unused code in NotFoundPage messages.js file

--- app/containers/NotFoundPage/messages.js
+++ app/containers/NotFoundPage/messages.js
@@ -1,8 +1,4 @@
-/*
- * NotFoundPage Messages
- *
- * This contains all the text for the NotFoundPage component.
- */
+
 import { defineMessages } from 'react-intl';
 
 export const scope = 'boilerplate.containers.NotFoundPage';
